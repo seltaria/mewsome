2d4fdded6af0452002a60f53a12797b10b7975d5
Бондарева Анастасия <seltaria@mail.ru>
2025-04-14 11:47:25 +0300

    feat: create project, add cat joke

---
+++ app/clicker/page.tsx
@@ -0,0 +1,3 @@
+export default function Clicker() {
+  return <div>Clicker game</div>;
+}
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['cdn2.thecatapi.com'],
+  },
+};
+
+export default nextConfig;
